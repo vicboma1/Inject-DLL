f24538a388b2b7fbe79fc0f198d59e2abc70afd7
Victor Bolinches <vicboma1@users.noreply.github.com>
2019-02-18 23:20:26 +0100

    Update ILoaderProcessNative.cs

--- Injects/injectDll/Loader/ILoaderProcessNative.cs
+++ Injects/injectDll/Loader/ILoaderProcessNative.cs
@@ -1,4 +1,8 @@
-﻿using System;
+using System;
+using winApi;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Collections.Generic;
 
 namespace Loader
 {
@@ -6,11 +10,27 @@ namespace Loader
     public interface ILoaderProcessNative
     {
         Boolean isLoaded { get; }
+        Boolean isActive();
+
+        int Open(String name);
+        Task OpenAsync(String name);
+
+        Kernel32Native.PROCESS_INFORMATION Create(String name, uint securityAttr);
+
+        Boolean Attach(String name);
         Boolean InjectDLL(String name);
-       
+
+        //static IntPtr Suspend(IntPtr process);
+        //static IntPtr Suspend();
+        List<IntPtr> SuspendAllThreads();
+          
         Boolean ReadMemory(IntPtr processID, IntPtr address, int numOfBytes, byte[] buffer, out int bytesRead);
         Boolean WriteMemory(IntPtr hProc, IntPtr address, byte [] buffer, out int bytesWrited);
 
-        uint Resume();
+        //static IntPtr ResumeThread(IntPtr pthread);
+        //static List<IntPtr> ResumeAllThreads(Kernel32Native.PROCESS_INFORMATION pi);
+        List<IntPtr> ResumeAllThreads();
+
+        Boolean Terminate();
     }
 }
